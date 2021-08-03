5b57acb370b3d2a13cb8e3752bed359c2461d0cd
root <root@root.com>
2021-08-03 23:30:43 +0000

    I finished dueling_buttons and calculator

--- _03_more_gui_apps/_b_dueling_buttons.py
+++ _03_more_gui_apps/_b_dueling_buttons.py
@@ -11,15 +11,25 @@ RIGHT_SMALL = {'relx' : 0.8, 'relwidth' : 0.2}
 # ======================= DO NOT EDIT THE CODE ABOVE =========================
 
 # TODO 1) Create a new tkinter class
+class DuelingButtons(tk.Tk):
 
     # TODO 2) Create a constructor
-
+    def __init__(self):
     # TODO 3) call Tk's constructor,
+        super().__init__()
 
     # TODO 4) Create 2 buttons next to each other, side by side, with the
     #  text "Click me!". Make sure the buttons are member variables and
     #  look at the example photo for reference.
+        self.button_one = tk.Button(self, text="Click me!", fg='blue',
+                                    font=('times new roman', 13, 'bold'), command=self.button_one_pressed)
 
+        self.button_one.place(relx=0, rely=0, relwidth=0.5, relheight=1)
+
+        self.button_two = tk.Button(self, text="Click me!", fg='blue',
+                                font=('times new roman', 13, 'bold'), command=self.button_two_pressed)
+
+        self.button_two.place(relx=0.5, rely=0, relwidth=0.5, relheight=1)
     # TODO 5) For the left button, add: command=self.left_button_pressed
     #  For the right button, add: command=self.right_button_pressed
 
@@ -27,15 +37,21 @@ RIGHT_SMALL = {'relx' : 0.8, 'relwidth' : 0.2}
     #  button to small and the right button to big. For example,
     #  self.button_left.place(LEFT_SMALL)
     #  self.button_right.place(RIGHT_BIG)
+    def button_one_pressed(self):
+        self.button_one.place(LEFT_SMALL)
+        self.button_two.place(RIGHT_BIG)
 
     # TODO 7) Create a method called right_button_pressed and place the left
     #  button to big and the right button to small
-
+    def button_two_pressed(self):
+        self.button_one.place(LEFT_BIG)
+        self.button_two.place(RIGHT_SMALL)
 
 # TODO 8) Create an if __name__ == '__main__': block
-
+if __name__ == '__main__':
     # TODO 9) Create an object of the dueling buttons class
-
+    duel = DuelingButtons()
     # TODO 10) Set the object's width and size using the geometry method
-
+    duel.geometry("500x300")
     # TODO 11) Call the object's mainloop method
+    duel.mainloop()
--- _03_more_gui_apps/_c_calculator.py
+++ _03_more_gui_apps/_c_calculator.py
@@ -6,3 +6,53 @@ import tkinter as tk
 # TODO Make a calculator app like the one shown in the calculator.png image
 #  located in this folder.
 #  HINT: you'll need: 2 text fields, 4 buttons, and 1 label
+
+class Calculator(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.text_field_one = tk.Entry(self)
+        self.text_field_one.place(relx=0.1, rely=0.1, relwidth=0.3, relheight=0.1)
+
+        self.text_field_two = tk.Entry(self)
+        self.text_field_two.place(relx=0.6, rely=0.1, relwidth=0.3, relheight=0.1)
+
+
+        self.label = tk.Label(self, bg='white', font=('comic sans', 20, 'normal'))
+        self.label.place(relx=0, rely=0.3, relwidth=1, relheight=0.1)
+
+
+        self.button_plus = tk.Button(self, text="Add", fg='green',
+                                    font=('times new roman', 13, 'bold'), command=self.add)
+        self.button_plus.place(relx=0, rely=0.5, relwidth=0.25, relheight=0.1)
+
+        self.button_minus = tk.Button(self, text="Subtract", fg='red',
+                                     font=('times new roman', 13, 'bold'), command=self.subtract)
+        self.button_minus.place(relx=0.25, rely=0.5, relwidth=0.25, relheight=0.1)
+
+        self.button_times = tk.Button(self, text="Multiply", fg='green',
+                                     font=('times new roman', 13, 'bold'), command=self.multiply)
+        self.button_times.place(relx=0.5, rely=0.5, relwidth=0.25, relheight=0.1)
+
+        self.button_divided = tk.Button(self, text="Divide", fg='red',
+                                     font=('times new roman', 13, 'bold'), command=self.divide)
+        self.button_divided.place(relx=0.75, rely=0.5, relwidth=0.25, relheight=0.1)
+
+
+    def add(self):
+        self.label.configure(text = str(float(self.text_field_one.get()) + float(self.text_field_two.get())))
+    def subtract(self):
+        self.label.configure(text = str(float(self.text_field_one.get()) - float(self.text_field_two.get())))
+    def multiply(self):
+        self.label.configure(text = str(float(self.text_field_one.get())*float(self.text_field_two.get())))
+    def divide(self):
+        self.label.configure(text = str(float(self.text_field_one.get())/float(self.text_field_two.get())))
+
+
+
+
+if __name__ == "__main__":
+    calc = Calculator()
+    calc.geometry("400x200")
+    calc.mainloop()
+
